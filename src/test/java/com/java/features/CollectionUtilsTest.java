655c2b5dd091d63f1d94b08f4353356100581127
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-07-31 00:14:36 +0300

    fixed

--- src/test/java/com/java/features/CollectionUtilsTest.java
+++ src/test/java/com/java/features/CollectionUtilsTest.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2016-2017 Sophos Limited. All rights reserved.
- *
- * 'Sophos' and 'Sophos Anti-Virus' are registered trademarks of Sophos Limited
- * and Sophos Group.  All other product and company names mentioned are
- * trademarks or registered trademarks of their respective owners.
- */
 package com.java.features;
 
 import org.junit.Test;
